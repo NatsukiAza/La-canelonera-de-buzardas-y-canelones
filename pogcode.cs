e3ba07c452c6d1705d5f2c3404ff86233ee0d3fc
Puchis <70609993+Puchis@users.noreply.github.com>
2020-09-03 07:58:41 -0300

    Revert "Update pogcode.cs"
    This reverts commit 562fadb5e084d6500e9efa672e8c9c03be6ab7e4.

--- pogcode.cs
+++ pogcode.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication9
     {
         static void Main(string[] args)
         {
-            int pm = 0, pt = 0, pn = 0, 
+            int pm = 0, pt = 0, pn = 0, v, ppm, ppt, ppn;
             for (int i = 1; i <= 69; i++)
             { 
             Console.WriteLine("Ingrese la edad de un estudiante del turno mañana");
